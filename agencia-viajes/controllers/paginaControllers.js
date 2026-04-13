7a4c8b1c51da51f1cf6a2b94b15ae66bede894e1
mike_MSI <later481@gmail.com>
2026-04-12 23:01:20 -0600

    feat: creacion de pagina detalles

--- agencia-viajes/controllers/paginaControllers.js
+++ agencia-viajes/controllers/paginaControllers.js
@@ -30,9 +30,26 @@ const paginaTestimoniales = (req, res) => {
     })
 }
 
+//Pagina de detalle de viaje
+const paginaDetalleViaje = async (req, res) => {
+    const { slug } = req.params;
+
+    try {
+        const viaje = await Viaje.findOne({ where : { slug } })
+        res.render('viaje', {
+            pagina: 'Información Viaje',
+            viaje
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
 export {
     paginaInicio,
     paginaNosotros,
     paginaViajes,
-    paginaTestimoniales
+    paginaTestimoniales,
+    paginaDetalleViaje
 }
